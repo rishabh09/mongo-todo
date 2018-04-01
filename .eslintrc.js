74349d1f3649829b4c25333d769e137f6a4ebf4d
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-04-01 20:13:37 +0530

    Setup dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "semi": 0,
+        "arrow-parens": [1, "as-needed"],
+        "comma-dangle": [0, "never"]
+    },
+    "env": {
+        "jest": true
+    }
+};
---
+++ src/server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(bodyParser.json())
+
+
+
+app.listen(PORT, () => {
+  console.log('server is running on port', PORT)
+})
