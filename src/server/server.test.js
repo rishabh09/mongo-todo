ecba4db6ccc3325f33875c39b331fd622b71eaee
Rishabh Mishra <mishrarishabh865@gmail.com>
2018-04-04 00:28:53 +0530

    Add test for GET /todos/:status

--- src/server/server.test.js
+++ src/server/server.test.js
@@ -7,6 +7,9 @@ describe('POST /create/todo', () => {
   beforeEach(done => {
     Todo.remove({}).then(() => done())
   })
+  afterEach(done => {
+    Todo.remove({}).then(() => done())
+  })
 
   it('should give error on sending invalid request', done => {
     const errorMsg = { requestStatus: 0, message: 'Error in creating todo' }
@@ -28,6 +31,51 @@ describe('POST /create/todo', () => {
       .expect(200)
       .expect(res => {
         expect(res.body).toEqual(successMsg)
+      })
+      .end(done)
+  })
+})
+
+describe('GET /todos/:status', () => {
+  const testDb = [
+    { name: 'test todo' },
+    { name: 'test todo 1' },
+    { name: 'test todo 1', completed: true }
+  ]
+  beforeEach(done => {
+    Todo.insertMany(testDb).then(() => done())
+  })
+
+  afterEach(done => {
+    Todo.remove({}).then(() => done())
+  })
+
+  it('should return list of all todos', done => {
+    request(app)
+      .get('/todos/all')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(3)
+      })
+      .end(done)
+  })
+
+  it('should return list of all completed todos', done => {
+    request(app)
+      .get('/todos/completed')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(1)
+      })
+      .end(done)
+  })
+
+  it('should return list of all incomplete todos', done => {
+    request(app)
+      .get('/todos/incomplete')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2)
       })
       .end(done)
   })
